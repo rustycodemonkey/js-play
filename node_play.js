a6c4a5ffc123321ed3bfda2a9ceb1a994d4412d6
Phillip Kim <philkim78@gmail.com>
2019-05-30 12:13:56 +1000

    Added new hello world files for a play

---
+++ node_play.js
@@ -0,0 +1,9 @@
+console.log("hello world")
+console.log("hello", "world")
+// alert('First', 'Second')
+console.log('First', 'Second')
+
+// alert(1)
+console.log(1)
+console.log(1.0)
+console.log('1')
